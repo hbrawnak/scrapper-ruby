e0b9dd056d2ef4fd10f95c1d3f525d7d5425e183
Habib <hbrawnak@gmail.com>
2017-11-23 12:33:21 +0600

    OOP Ruby

--- list.rb
+++ list.rb
@@ -1,20 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://bikroy.com/bn/ads/bangladesh/electronics"
+class WebScraper
+	def bikroy(url)
+		document = open(url)
+		content = document.read
+		parsed_content = Nokogiri::HTML(content)
 
-document = open(url)
-content = document.read
-parsed_content = Nokogiri::HTML(content)
-
-parsed_content.css('.ui-panel-content').css('.row').css('.ui-item').each do |row|
-	title = row.css('.item-title').inner_text
-	location = row.css('.item-area').inner_text
-	category = row.css('.item-cat').inner_text
-	price = row.css('.item-info').inner_text
-	puts title
-	puts location
-	puts category
-	puts price
-	puts '------------------------'
+		parsed_content.css('.ui-panel-content').css('.row').css('.ui-item').each do |row|
+			title = row.css('.item-title').inner_text
+			location = row.css('.item-area').inner_text
+			category = row.css('.item-cat').inner_text
+			price = row.css('.item-info').inner_text
+			puts title
+			puts location
+			puts category
+			puts price
+			puts '------------------------'
+		end
+	end
 end
+
+url = "https://bikroy.com/bn/ads/bangladesh/electronics?page=2"
+
+scrap = WebScraper.new()
+puts scrap.bikroy(url)
